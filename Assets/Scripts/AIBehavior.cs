bf2fae6cd0faae62f90b657540ce6408d3ebbc3e
daniel-petty <daniel.petty94@gmail.com>
2019-07-13 11:50:43 -0400

    Fireball

---
+++ Assets/Scripts/AIBehavior.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class AIBehavior : MonoBehaviour
+{
+    public GameObject moveToTarget;
+    public GameObject shootingTarget;
+    public GameObject projectilePrefab;
+    
+    private NavMeshAgent navMeshAgent;
+
+    void Awake(){
+        navMeshAgent = GetComponent<NavMeshAgent>();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        MoveTo(moveToTarget);
+    }
+
+    void MoveTo(GameObject target){
+        navMeshAgent.destination = target.transform.position;
+    }
+}
