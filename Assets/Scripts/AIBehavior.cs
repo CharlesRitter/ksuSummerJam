f1ad6c5f52ee46b5b7ea79602cbf57087834b786
daniel-petty <daniel.petty94@gmail.com>
2019-07-13 19:33:57 -0400

    Snowman blow

--- Assets/Scripts/AIBehavior.cs
+++ Assets/Scripts/AIBehavior.cs
@@ -2,17 +2,30 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
+using Lean.Pool;
 
 public class AIBehavior : MonoBehaviour
 {
-    public GameObject moveToTarget;
-    public GameObject shootingTarget;
-    public GameObject projectilePrefab;
-    
-    private NavMeshAgent navMeshAgent;
+    public GameObject target;
+    public ParticleSystem partSys;
+    public Transform blowRangeTransform;
+    public float rotationSpeed;
+    public float blowForce = 50f;
+    public bool blowing;
 
-    void Awake(){
+
+    NavMeshAgent navMeshAgent;
+    GameObject player;
+    bool inBlowRange;
+    bool melting;
+    float blowRange = 20f;
+
+    void Awake()
+    {
         navMeshAgent = GetComponent<NavMeshAgent>();
+        partSys = GetComponentInChildren<ParticleSystem>();
+        player = GameObject.FindGameObjectWithTag("Player");
+        blowRange = Vector3.Distance(transform.position, blowRangeTransform.position);
     }
 
     // Start is called before the first frame update
@@ -23,10 +36,58 @@ public class AIBehavior : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        MoveTo(moveToTarget);
+        if (melting)
+        {
+            //StartMelting();
+        }
+        else
+        {
+
+            if (blowing) FaceTarget(target.transform.position);
+
+            if (GetPlayerDistance() < blowRange)
+                StartBlowing();
+            else
+            {
+                if (blowing) StopBlowing();
+                MoveTo(player.transform.position);
+            }
+        }
     }
 
-    void MoveTo(GameObject target){
-        navMeshAgent.destination = target.transform.position;
+    void MoveTo(Vector3 target)
+    {
+        navMeshAgent.destination = target;
     }
+
+    public void StartBlowing()
+    {
+        blowing = true;
+        partSys.Play();
+    }
+
+    void StopBlowing()
+    {
+        blowing = false;
+        partSys.Stop();
+    }
+
+    void FaceTarget(Vector3 destination)
+    {
+        Vector3 lookPos = destination - transform.position;
+        lookPos.y = 0;
+        Quaternion rotation = Quaternion.LookRotation(lookPos);
+        transform.rotation = Quaternion.Slerp(transform.rotation, rotation, rotationSpeed);
+    }
+
+    float GetPlayerDistance()
+    {
+        return Vector3.Distance(player.transform.position, transform.position);
+    }
+
+    public void Melt()
+    {
+        melting = true;
+    }
+
 }
