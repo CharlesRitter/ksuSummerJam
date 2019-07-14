39420db17ebf99e34c2826233d113f45547f5ef6
ColtonS <colton3@g.clemson.edu>
2019-07-14 13:35:00 -0400

    added snow particles, vines, vine gate

---
+++ Assets/AssetsToImport/Gate/GateManager.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GateManager : MonoBehaviour
+{
+
+    public List<GameObject> vineList;
+    // Use this for initialization
+    public bool vinesToggle, vinesState, hasBeenUnlocked;
+    public GameObject padlock;
+    public float vineTimeOffset;
+
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (vinesToggle)
+        {
+
+            if (!hasBeenUnlocked)
+            {
+                if (padlock.GetComponent<DestroyPadlock>().isDestroyed != true)
+                {
+                    padlock.GetComponent<DestroyPadlock>().isDestroyed = true;
+                    StartCoroutine(OffsetVineAnimation());
+                }
+
+
+                hasBeenUnlocked = true;
+            }
+
+            else
+            {
+                if (vinesState)
+                {
+                    foreach (GameObject vine in vineList)
+                    {
+                        vine.GetComponentInChildren<VineAnim>().growth = true;
+
+                    }
+                }
+
+                else
+                {
+                    foreach (GameObject vine in vineList)
+                    {
+                        vine.GetComponentInChildren<VineAnim>().growth = false;
+
+                    }
+                }
+            }
+
+            vinesState = !vinesState;
+            vinesToggle = false;
+        }
+
+        IEnumerator OffsetVineAnimation()
+        {
+            yield return new WaitForSeconds(vineTimeOffset);
+            foreach (GameObject vine in vineList)
+            {
+                vine.GetComponentInChildren<VineAnim>().growth = false;
+
+            }
+            yield return null;
+        }
+    }
+}
---
+++ Assets/AssetsToImport/Padlock/DestroyPadlock.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyPadlock : MonoBehaviour
+{
+    public Rigidbody Padlock_Handle, Padlock_Base;
+    public Vector3 forceToApply1, forceToApply2;
+    public float waitTime1, waitTime2;
+
+    public bool isDestroyed = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isDestroyed)
+        {
+            StartCoroutine(PadlockDamage());
+        }
+    }
+
+    IEnumerator PadlockDamage()
+    {
+        Padlock_Base.isKinematic = false;
+        Padlock_Base.useGravity = true;
+        yield return new WaitForSeconds(waitTime1);
+
+        Padlock_Handle.isKinematic = false;
+        Padlock_Handle.useGravity = true;
+        yield return new WaitForSeconds(waitTime2);
+
+        Destroy(Padlock_Base.gameObject);
+        Destroy(Padlock_Handle.gameObject);
+
+        yield return null;
+    }
+}
