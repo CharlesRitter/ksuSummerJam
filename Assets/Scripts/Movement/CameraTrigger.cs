e79cec9da9cb7fef15739a7b9e15b50097c584cb
Charles Ritter <crritte@g.clemson.edu>
2019-07-14 01:11:31 -0400

    added death triggers and camera transitions

--- Assets/Scripts/Movement/CameraTrigger.cs
+++ Assets/Scripts/Movement/CameraTrigger.cs
@@ -4,15 +4,36 @@ using UnityEngine;
 
 public class CameraTrigger : MonoBehaviour
 {
+    GameObject mainCamera;
+    bool inTransition = false;
+    public float cameraStop;
     // Start is called before the first frame update
     void Start()
     {
-        
+        mainCamera = GameObject.Find("Main Camera");
     }
 
     // Update is called once per frame
     void Update()
     {
+
+        if (inTransition)
+        {
+            mainCamera.transform.Translate(new Vector3(0, 0, 30) * Time.deltaTime, Space.World);
+            if (mainCamera.transform.position.z >= cameraStop)
+            {
+                inTransition = false;
+            }
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.transform.gameObject.tag == "Player")
+        {
+            inTransition = true;
+            gameObject.GetComponent<BoxCollider>().enabled = false;
+        }
         
     }
 }
