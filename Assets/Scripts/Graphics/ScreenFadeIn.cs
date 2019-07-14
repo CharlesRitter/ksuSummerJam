3fcdeeda231495eea89093e5dc97f4c20b358001
Charles Ritter <crritte@g.clemson.edu>
2019-07-14 01:36:24 -0400

    Merged title_branch to master on Charles Ritter

---
+++ Assets/Scripts/Graphics/ScreenFadeIn.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScreenFadeIn : MonoBehaviour
+{
+    public float fadeTime;
+    public float fadeWait;
+
+    private Image img;
+
+    void Start()
+    {
+        img = GetComponent<Image>();
+        StartCoroutine(FadeScreen(fadeTime));
+    }
+    
+    private IEnumerator FadeScreen(float time)
+    {
+        yield return new WaitForSeconds(fadeWait);
+        float current = img.color.a;
+        for(float t = 0.0f; t < 1.0f; t += Time.deltaTime / time)
+        {
+            Color col = new Color(img.color.r, img.color.g, img.color.b, Mathf.Lerp(current, 0.0f, t));
+            img.color = col;
+            yield return null;
+        }
+        gameObject.SetActive(false);
+    }
+}
---
+++ Assets/Scripts/PlayerDeath.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerDeath : MonoBehaviour
+{
+    Transform spawn;
+    public void Respawn()
+    {
+        gameObject.transform.position = spawn.position;
+    }
+}
